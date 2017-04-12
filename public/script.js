a5158b7e730cc950fb04bd3edd452254ffa86668
Kristyn Ginski <Oberst54!>
2017-04-11 21:05:43 -0400

    fix routes

--- public/script.js
+++ public/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
-// pass in input. input = location on db
+// input = location from db
   let getWeather = function(){
+    let input = 'New York';
+
     $.ajax({
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Miami&units=imperial&APPID=142c478807cfdf791bc211f12745a186',
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${input}&units=imperial&APPID=142c478807cfdf791bc211f12745a186`,
       method: 'GET',
       dataType: 'json',
         success: function(data){
@@ -24,16 +26,19 @@ $(document).ready(function(){
       // }
 
   } // ends getWeather
-  getWeather();
+  getWeather()
 
 
 
 // getProduct(garment_name) <--- pass in garment_name from table
 // can you pass a db value to a function? of course..
 
-  let getProduct = function(){
+  let getProduct = function(data){
+    let offset_input = (Math.floor(Math.random() * (1000 - 1)) + 1)
+    let api_input = 'ankle+boots'
+
     $.ajax({
-      url: 'http://api.shopstyle.com/api/v2/products?pid=uid3841-22532279-49&format=JSON&fts=ankle+boots&offset=' + (Math.floor(Math.random() * (50 - 1)) + 1) +'&limit=1',
+      url: `http://api.shopstyle.com/api/v2/products?pid=uid3841-22532279-49&format=JSON&fts=${api_input}&offset=${offset_input}&limit=1`,
       method: 'GET',
       dataType: 'json',
         success: function(data){
@@ -43,6 +48,7 @@ $(document).ready(function(){
           let price = data.products[0].priceLabel;
           let product_img = data.products[0].image.sizes.Large.url;
           console.log(data, product_name, product_img, click_url, price);
+
           $('.garment_today').text(`It's a nice day to wear xxxxxxx !`)
           $('h2.brand').text(product_name);
           $('p.title').text(title)
@@ -52,9 +58,7 @@ $(document).ready(function(){
       }
     })
   }
-
-  getProduct()
-
+getProduct()
 
 
 
